29116ada81610b22dc52ab75b74c006828525fd7
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-14 21:09:07 +0700

    Task08 added

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int counter = 2;
+while (counter <= num)
+{
+    Console.Write(counter);
+    
+    counter += 2;
+
+    if (counter <= num)
+    {
+        Console.Write(", ");
+    }
+
+    
+}
