5be0e0e85b88bc02fbca162686d6f59a6d26d486
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-14 20:59:15 +0700

    Task06 added

---
+++ Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 == 0){
+    Console.WriteLine("It's an even number");
+} else {
+    Console.WriteLine("It's an odd number");
+}
