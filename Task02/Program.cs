94d691811cfdeb5a242f2477ee565b388c64448b
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-14 20:40:09 +0700

    Task02 added

---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a < b) {
+    max = b;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
