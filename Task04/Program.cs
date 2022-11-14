f7acfebe1992e3b7a55044b83a12bf07ba826aac
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-14 20:52:26 +0700

    Task04 added

---
+++ Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿int counter = 1;
+int max = int.MinValue;
+while (counter <= 3)
+{
+    Console.Write("Enter number:");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > max) {max = num;}
+
+    counter++;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
